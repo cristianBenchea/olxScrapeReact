f41c4ac1e015ecdecfc8ecaecec1d07e7fc2aea6
Cristian <cristian.benchea781@yahoo.com>
2023-07-11 00:38:06 +0300

    add tailwind

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 import Anunt from './components/Anunt/Anunt';
 import Header from './components/Header/Header';
 import favoritesJSON from './scrapedFavorites.json';
-import anunturiJSON from './anunturi.json';
+import anunturiJSON from './scrapedAnunturi.json';
 
 function App() {
   const [anunturiArray, setAnunturi] = useState(favoritesJSON);
-  
 
   const sortBy = (arr, ascDesc, field) => {
     const sorted = arr.sort((a, b) => {
@@ -53,23 +52,25 @@ function App() {
   return (
     <div className="App">
         <Header />
-        <div className='buttons'>
-          <button onClick={() => sortBy([...anunturiArray], 'asc', 'price')}>Sort price Asc</button>
-          <button onClick={() => sortBy([...anunturiArray], 'desc', 'price')}>Sort price Desc</button>
-          <button onClick={() => sortBy([...anunturiArray], 'asc', 'year')}>Sort Year Asc</button>
-          <button onClick={() => sortBy([...anunturiArray], 'desc', 'year')}>Sort Year Desc</button>
-          <button onClick={() => sortBy([...anunturiArray], 'asc', 'km')}>Sort KM Asc</button>
-          <button onClick={() => sortBy([...anunturiArray], 'desc', 'km')}>Sort KM Desc</button>
+        <div className='container mx-auto'>
+          <div className='flex justify-center space-x-4 font-sans mt-4'>
+            <button className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={() => sortBy([...anunturiArray], 'asc', 'price')}>Sort price Asc</button>
+            <button className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={() => sortBy([...anunturiArray], 'desc', 'price')}>Sort price Desc</button>
+            <button className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={() => sortBy([...anunturiArray], 'asc', 'year')}>Sort Year Asc</button>
+            <button className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={() => sortBy([...anunturiArray], 'desc', 'year')}>Sort Year Desc</button>
+            <button className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={() => sortBy([...anunturiArray], 'asc', 'km')}>Sort KM Asc</button>
+            <button className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={() => sortBy([...anunturiArray], 'desc', 'km')}>Sort KM Desc</button>
+          </div>
+          <div className='flex justify-center space-x-4 font-sans mt-4'>
+            <button className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={() => changeAnunturi("favorites")}>Favorites</button>
+            <button className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={() => changeAnunturi("anunturi")}>Anunturi</button>
+          </div>
+          <div className="flex justify-center flex-col space-y-8 font-sans mt-8">
+            {anunturiArray.map((key, i) => (
+              <Anunt key={i} title={key.title} price={key.price} thumbnail={key.thumbnail} link={key.link} yearAndKm={key.yearAndKm} locationAndDate={key.locationAndDate} />
+            ))}
+          </div> 
         </div>
-        <div className='categorii'>
-          <button onClick={() => changeAnunturi("favorites")}>Favorites</button>
-          <button onClick={() => changeAnunturi("anunturi")}>Anunturi</button>
-        </div>
-        <div className="anunturi">
-          {anunturiArray.map((key, i) => (
-            <Anunt key={i} title={key.title} price={key.price} thumbnail={key.thumbnail} link={key.link} yearAndKm={key.yearAndKm} locationAndDate={key.locationAndDate} />
-          ))}
-        </div> 
     </div>
   );
 }
--- src/components/Anunt/Anunt.js
+++ src/components/Anunt/Anunt.js
@@ -3,11 +3,11 @@ import './Anunt.css';
 function Anunt({title, price, thumbnail, link, yearAndKm, locationAndDate}) {
   
   return (
-    <div className='anunt'>
+    <div className='flex'>
       <div className='thumbnail'>
-        <img src={thumbnail} alt={title} />
+        <img src={thumbnail} alt={title} className='w-60 h-48 object-cover'/>
       </div>
-      <div className='detaliiAnunt'>
+      <div className='ml-10'>
         <div className='detaliiZ'>
           <h3><a href={link} target='_blank' rel="noreferrer">{title}</a></h3>
           <div>
